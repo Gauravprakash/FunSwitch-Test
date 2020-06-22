9235c7194a475f0cdef1d9d0bd4d2e293eb4e3d3
Gaurav Prakash <gauravprakash@mediologysoftware.com>
2020-06-22 21:20:27 +0530

    Initial Commit

---
+++ FunSwitch Test/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FunSwitch Test
+//
+//  Created by gaurav on 22/06/20.
+//  Copyright © 2020 mackbook. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
